b417bf2ddfa9e76efdbb192b1769cf847821fc3b
yha7 <hayuvaraj90@gmail.com>
2020-12-08 22:02:59 +0530

    Initial verion commit

---
+++ spark/scala/app-ts-generate-sessionid/src/main/scala/logic/BootStrap.scala
@@ -0,0 +1,18 @@
+package logic
+import org.apache.spark.sql.SparkSession
+import org.apache.spark.sql
+
+object BootStrap extends App {
+
+  val spark = SparkSession
+    .builder()
+    .appName("Spark SQL basic example")
+    .config("spark.master", "local[2]")
+    .getOrCreate()
+
+  // For implicit conversions like converting RDDs to DataFrames
+  import spark.implicits._
+  val df = spark.sparkContext.parallelize(Seq(1, 2, 3, 4, 5)).toDF("Number")
+  df.printSchema()
+  df.show()
+}
