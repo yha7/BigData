654a2f98f54096f36ef640d176ca98be68b2809f
yha7 <hayuvaraj90@gmail.com>
2020-12-16 21:58:01 +0530

    Updated the code for creation of a new session id for a clickstream data.

--- spark/scala/app-ts-generate-sessionid/src/main/scala/logic/BootStrap.scala
+++ spark/scala/app-ts-generate-sessionid/src/main/scala/logic/BootStrap.scala
@@ -1,6 +1,10 @@
 package logic
 import org.apache.spark.sql.SparkSession
-import org.apache.spark.sql
+import org.apache.spark.sql._
+//import org.apache.spark.sql.catalyst.plans.logical.Window
+import org.apache.spark.sql.functions._
+import org.apache.spark.sql.types._
+import org.apache.spark.sql.expressions.Window
 
 object BootStrap extends App {
 
@@ -12,7 +16,28 @@ object BootStrap extends App {
 
   // For implicit conversions like converting RDDs to DataFrames
   import spark.implicits._
-  val df = spark.sparkContext.parallelize(Seq(1, 2, 3, 4, 5)).toDF("Number")
-  df.printSchema()
-  df.show()
+
+  val rawDataframe = Seq(("2018-01-01 11:00:00","u1"),
+  ("2018-01-01 12:10:00","u1"),
+  ("2018-01-01 13:00:00","u1"),
+  ("2018-01-01 13:25:00","u1"),
+  ("2018-01-01 14:40:00","u1"),
+  ("2018-01-01 15:10:00","u1"),
+  ("2018-01-01 16:20:00","u1"),
+  ("2018-01-01 16:50:00","u1"),
+  ("2018-01-01 11:00:00","u2"),
+  ("2018-01-02 11:00:00","u2") ).toDF("ts","user_id")
+
+  val df = rawDataframe.withColumn("ts_converted", to_timestamp(col("ts"),"yyyy-MM-dd HH:mm:ss"))
+    .withColumn("ts_lag",lag(col("ts_converted"),1)
+      .over(Window.partitionBy(col("user_id")).orderBy("ts")))
+
+  val df1 =df.withColumn("uid",
+    when(col("ts_lag").isNull,concat(col("user_id"),col("ts_converted")))
+      .when((unix_timestamp(col("ts_converted"))-unix_timestamp(col("ts_lag")))/(60) >= 30 ,
+        concat(col("user_id"),col("ts_converted")))
+          .otherwise(concat(col("user_id"),col("ts_lag"))
+  ))
+  df1.show(false)
+  df1.printSchema()
 }
